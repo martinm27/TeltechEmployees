79ea04e6341dd465618da6a89b563813851c582f
martinmihalic <martin.mihalic@five.agency>
2021-02-11 00:57:42 +0100

    Network module and API specification

---
+++ network/src/main/java/com/teltech/network/di/NetworkModule.kt
@@ -0,0 +1,42 @@
+package com.teltech.network.di
+
+import com.teltech.network.service.EmployeesService
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val API_URL = "https://teltech.co/"
+
+fun networkModule(): Module = module {
+
+    single<Interceptor> {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    single<OkHttpClient> {
+        OkHttpClient.Builder().apply {
+            connectTimeout(100, TimeUnit.SECONDS)
+            readTimeout(100, TimeUnit.SECONDS)
+            addInterceptor(get())
+        }
+            .build()
+    }
+
+    single<EmployeesService> {
+        Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+            .create(EmployeesService::class.java)
+    }
+}
---
+++ network/src/main/java/com/teltech/network/service/EmployeesService.kt
@@ -0,0 +1,11 @@
+package com.teltech.network.service
+
+import com.teltech.network.model.APIEmployee
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface EmployeesService {
+
+    @GET("/teltechiansFlat.json")
+    fun getProducts(): Single<List<APIEmployee>>
+}
